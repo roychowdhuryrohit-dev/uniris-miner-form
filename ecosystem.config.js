108dff8c4f1e6e4a9e5791406ef6d81fbbcd70ee
roychowdhuryrohit-dev <roychowdhuryrohit.dev@gmail.com>
2021-06-28 03:04:25 +0530

    Added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [{
+        name: "miner-form",
+        script: "./app.js",
+        env: {
+            NODE_ENV: "development",
+        },
+        env_production: {
+            NODE_ENV: "production",
+        },
+        node_args: "-r dotenv/config",
+        watch: true,
+        ignore_watch: ["node_modules", ".env", ".gitignore", "package.json", "data"],
+        watch_options: {
+            followSymlinks: false,
+        },
+        kill_timeout: 10000,
+        treekill: false
+    }]
+};
